04dc63a92fd5e021b32d3d88284578f4091f63d8
eloragudi <gudielora@gmail.com>
2022-04-23 04:45:59 -0700

    nuesauniuyo

--- src/pages/history.jsx
+++ src/pages/history.jsx
@@ -1,6 +1,22 @@
 import React from "react"
 export default function Historypage() {
     return (
-        <div className=""></div>
+        <div className="">
+            <section className="section">
+                <div className="container mx-auto px-4">
+                <h2 className="section-title">
+                    History of UNIUYO
+                </h2>
+                <div className="">
+                    <p className="">
+                        The University of Uyo was founded in 1991 by the Federal Government of Nigeria. At the time, there existed in Uyo the University of Cross River State which was established in 1981 by the then Cross River State Government. In 1987 Akwa Ibom State with headquarters at Uyo was created out of the Cross River State and from that time up to the time of Federal Government take-over in 1991, the University was jointly owned by Akwa Ibom and Cross River State Governments.
+                    </p>
+                    <p>
+                    When first proposed by the President, and Commander-in-Chief of the Armed Forces, the University was to be a Federal University of Technology (FUT) and the Campus of the University of Cross River State at Uyo was to be the nucleus of the proposed University.
+                    </p>
+                </div>
+                </div>
+            </section>
+        </div>
     )
 }
